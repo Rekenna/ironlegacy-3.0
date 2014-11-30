eb907da13050b32f7e5de5367534764b5387b8cf
Ryan P McKenna <ryan@rekenna.com>
2014-11-30 13:31:29 -0700

    JQuery CDN

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,2 @@
 Rails.application.config.assets.precompile += %w( forem.css )
 Rails.application.config.assets.precompile += %w( forem.js )
-Rails.application.config.assets.precompile += %w( jquery-2.1.1.min.js )
