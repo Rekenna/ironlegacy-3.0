408547ba736c8f4ef323aa39abda3f313a17f3f6
Ryan P McKenna <ryan@rekenna.com>
2014-11-29 23:11:47 -0700

    Once more for the Jquery link

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( forem.css )
 Rails.application.config.assets.precompile += %w( forem.js )
+Rails.application.config.assets.precompile += %w( jquery-2.1.1.min.js )
