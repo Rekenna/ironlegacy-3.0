a03d1f2002d322618b44398d934b0ca4970a544e
Ryan P McKenna <ryan@rekenna.com>
2014-12-02 15:13:04 -0700

    Updated Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app32079330@heroku.com'],
-  :password       => ENV['5x47fyam'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+config.action_mailer.default_url_options[:host]
 
 end
