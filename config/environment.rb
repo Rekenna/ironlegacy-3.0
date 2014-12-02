645454756b314d4495c1c9b1c761b3c7a281aaa0
Ryan P McKenna <ryan@rekenna.com>
2014-12-02 15:31:28 -0700

    Updated Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,3 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['app32079330@heroku.com'],
-  :password       => ENV['5x47fyam'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
