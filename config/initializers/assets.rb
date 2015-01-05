870081d2dbd673fb0e87218aa20f7b9984421d5a
Ryan <ryan@rekenna.com>
2015-01-04 23:56:10 -0700

    Static Assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,4 @@
 Rails.application.config.assets.precompile += %w( forem.css )
 Rails.application.config.assets.precompile += %w( forem.js )
-Rails.application.config.assets.precompile += %w( sweet-alert.css )
-Rails.application.config.assets.precompile += %w( normalize.css )
+
 
