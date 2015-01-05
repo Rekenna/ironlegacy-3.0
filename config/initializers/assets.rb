58222c1acb38db40a12addbf5bb7986af4e9d612
Ryan <ryan@rekenna.com>
2015-01-04 23:35:24 -0700

    Tree

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,5 @@
 Rails.application.config.assets.precompile += %w( forem.css )
 Rails.application.config.assets.precompile += %w( forem.js )
+Rails.application.config.assets.precompile += %w( sweet-alert.css )
+Rails.application.config.assets.precompile += %w( normalize.css )
+
