bc0dbb774768e22a04b657290aabddd6f9ac4e7d
Ryan <ryan@rekenna.com>
2015-01-08 17:26:27 -0700

    Made success messages better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  @recent_posts = Forem::Post.all
+
 end
--- app/decorators/controllers/forem/application_controller_decorator.rb
+++ app/decorators/controllers/forem/application_controller_decorator.rb
@@ -1,3 +1,4 @@
 Forem::ApplicationController.class_eval do
   # before_filter :authenticate_user!
+  
 end
